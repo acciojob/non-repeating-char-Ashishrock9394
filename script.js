fc3d99c99f79a530d77f533945d0613a1ae55f13
Ashish Kumar <48367301+Ashishrock9394@users.noreply.github.com>
2023-09-30 14:07:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+ let count={};    
+    for(let i=0;i<str.length;i++){
+		
+        if(count[str.charAt(i)]==undefined)
+            count[str.charAt(i)]=1;
+        else
+            count[str.charAt(i)]+=1;
+    }
+    let i=0;
+    let ans=null;
+    for (const key in count) {
+        if (count.hasOwnProperty(key)) {
+            const value = count[key];
+        } 
+        if(count[key]==1){
+            ans=key;
+            break;
+        }
+    }
+    return ans;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+console.input = prompt("Enter a string"); 
+alert(firstNonRepeatedChar(input));
